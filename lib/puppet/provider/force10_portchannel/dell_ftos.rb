b1477c593b27241517b67eef8c0e200fb6b33613
bharath kumar4 <bharath_kumar4@dell.com>
2013-12-23 12:07:23 +0000

    Code changes for Force10 Port Channel resource

---
+++ lib/puppet/provider/force10_portchannel/dell_ftos.rb
@@ -0,0 +1,22 @@
+require 'puppet/provider/dell_ftos'
+
+
+Puppet::Type.type(:force10_portchannel).provide :dell_ftos, :parent => Puppet::Provider::Dell_ftos do
+  
+  desc "Dell force10 switch provider for port channel configuration."
+  
+  mk_resource_methods
+
+  def initialize(device, *args)
+    super
+  end
+
+  def self.lookup(device, name)
+    device.switch.portchannel(name).params_to_hash
+  end
+
+  def flush
+    device.switch.portchannel(name).update(former_properties, properties)
+    super
+  end
+end
---
+++ lib/puppet/type/force10_portchannel.rb
@@ -0,0 +1,41 @@
+Puppet::Type.newtype(:force10_portchannel) do
+  @doc = "This represents a VLAN configuration on a Dell force10 switch."
+
+  apply_to_device
+
+  ensurable
+
+  newparam(:name) do
+    isnamevar
+    newvalues(/^\d+$/)
+
+    validate do |value|
+      return if value == :absent
+      raise ArgumentError, "'Port Channel' value  must be between 2-128" unless value.to_i >1 &&	value.to_i <= 128
+    end
+ 
+  end
+
+
+
+  newproperty(:desc) do
+    newvalues(/^\S+$/)
+  end
+
+
+  newproperty(:mtu) do
+    newvalues(/^\d+$/)
+
+    validate do |value|
+      return if value == :absent
+      raise ArgumentError, "'MTU' value  must be between 2-128" unless value.to_i >=594 && value.to_i <= 12000
+    end
+  end
+
+  newproperty(:shutdown) do
+    defaultto(:false)
+    newvalues(:false,:true)
+  end
+
+
+end
