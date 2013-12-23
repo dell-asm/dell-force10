613111cea772f4cabf30eb36757f2c19a55b34bf
bharath kumar4 <bharath_kumar4@dell.com>
2013-12-23 12:23:14 +0000

    Code changes for Force10 Port Channel resource(Description changes)

--- lib/puppet/type/force10_portchannel.rb
+++ lib/puppet/type/force10_portchannel.rb
@@ -11,7 +11,7 @@ Puppet::Type.newtype(:force10_portchannel) do
 
     validate do |value|
       return if value == :absent
-      raise ArgumentError, "'Port Channel' value  must be between 2-128" unless value.to_i >1 &&	value.to_i <= 128
+      raise ArgumentError, "'Port Channel' value  must be between 1-128" unless value.to_i >=1 &&	value.to_i <= 128
     end
  
   end
@@ -28,7 +28,7 @@ Puppet::Type.newtype(:force10_portchannel) do
 
     validate do |value|
       return if value == :absent
-      raise ArgumentError, "'MTU' value  must be between 2-128" unless value.to_i >=594 && value.to_i <= 12000
+      raise ArgumentError, "'MTU' value  must be between 594-12000" unless value.to_i >=594 && value.to_i <= 12000
     end
   end
 
