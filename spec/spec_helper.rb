b2b656f4fe6bd766462596a37cbbabe835fa89d1
samir pattanaik <samir_pattanaik@dell.com>
2014-01-09 10:55:41 +0000

    Added rspec files for force10 firmware update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,27 @@
-require 'rubygems'
-require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet'
+require 'spec_helper'
+require 'yaml'
+require 'puppet/provider/dell_ftos'
+require 'puppet/util/network_device/dell_ftos/device'
+require 'puppet/util/network_device/base_ftos'
+require 'rspec/mocks'
+require 'puppet/provider/dell_ftos'
+
+module_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..','lib'))
+
+RSpec.configure do |c|
+  c.module_path = module_path
+  c.manifest_dir = File.join(module_path, 'manifests')
+
+  if Puppet::Util::Platform.windows?
+    c.output_stream = $stdout
+    c.error_stream = $stderr
+
+    c.formatters.each do |f|
+      if not f.instance_variable_get(:@output).kind_of?(::File)
+        f.instance_variable_set(:@output, $stdout)
+      end
+    end
+  end
+end
---
+++ spec/unit/puppet/type/force10_firmwareupdate_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:force10_firmwareupdate) do
+  let(:title) { 'force10_firmwareupdate' }
+
+   let :resource do
+    described_class.new(
+		:name          		=> 'firmwareupdate',
+		:firmwarelocation    	=> 'tftp://172.152.0.89/Force10/FTOS-SE-9.2.0.2.bin',
+		:forceupdate		=> false,
+    )
+   end
+
+  context 'should compile with given test params' do
+    it do
+      expect {
+        should compile
+      }
+    end
+  end
+
+    it "should have name as one of its parameters" do
+      described_class.key_attributes.should == [:name]
+    end 
+    
+    describe "when validating attributes" do   
+      [:name,:forceupdate].each do |param|
+	 it "should hava a #{param} parameter" do
+        described_class.attrtype(param).should == :param
+      end
+     end
+    [:firmwarelocation].each do |property|
+     it "should have a #{property} property" do
+        described_class.attrtype(property).should == :property
+      end
+    end	  
+  end
+end
+
