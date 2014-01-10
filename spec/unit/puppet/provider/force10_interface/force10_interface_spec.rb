2c74487fc950790fe3c8e86a68972403bbdd5537
umesha bhat <umesha_bhat@dell.com>
2014-01-10 11:30:16 +0000

    Rspec changes formatted the code for  force10

--- spec/unit/puppet/provider/force10_interface/force10_interface_spec.rb
+++ spec/unit/puppet/provider/force10_interface/force10_interface_spec.rb
@@ -1,21 +1,16 @@
 #! /usr/bin/env ruby
 require 'spec_provider_helper'
-
 require 'puppet/provider/force10_interface/dell_ftos'
 
 provider_class = Puppet::Type.type(:force10_interface).provider(:dell_ftos)
 
-
-
 describe provider_class do
-
 
   before do
     @interface = stub_everything 'interface'
     @interface.stubs(:name).returns('te 0/6')
     @interface.stubs(:params_to_hash)
     @interfaces = [ @interface ]
-
 
     @switch = stub_everything 'switch'
     @switch.stubs(:interface).returns(@interfaces)
@@ -28,9 +23,7 @@ describe provider_class do
 
     @provider = provider_class.new(@device, @resource)
 
-
-  end  
-
+  end
 
   it "should have a parent of Puppet::Provider::Dell_ftos" do
     provider_class.should < Puppet::Provider::Dell_ftos
@@ -50,9 +43,8 @@ describe provider_class do
       @switch.expects(:interface).with('te 0/6').returns(@interface)
       @interface.expects(:params_to_hash)
       provider_class.lookup(@device, 'te 0/6')
-    end   
+    end
 
- 
     it "should return the given configuration data" do
       @device.expects(:switch).returns(@switch)
       @switch.expects(:interface).with('te 0/6').returns(@interface)
@@ -70,14 +62,13 @@ describe provider_class do
       @switch.expects(:interface).with('te 0/6').returns(@interface)
       @switch.stubs(:facts).returns({})
       @interface.expects(:update).with({:ensure => :present, :name => 'te 0/6', :mtu => '1110'},
-                                  {:ensure => :present, :name => 'te 0/6', :mtu => '1110'})
+      {:ensure => :present, :name => 'te 0/6', :mtu => '1110'})
       @interface.expects(:update).never
 
       #@instance.desc = "FOOBAR"
       @instance.flush
     end
   end
-
 
 end
 
