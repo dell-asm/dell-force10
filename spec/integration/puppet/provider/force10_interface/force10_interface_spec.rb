f97cd302660481c6d6b33d1806806dfe9576ecb3
uvbhat11 <umesha_bhat@dell.com>
2014-01-21 10:27:16 +0530

    RSpec integration for force 10. Formatted and removed commented code

--- spec/integration/puppet/provider/force10_interface/force10_interface_spec.rb
+++ spec/integration/puppet/provider/force10_interface/force10_interface_spec.rb
@@ -5,21 +5,15 @@ require 'yaml'
 require 'puppet/util/network_device/dell_ftos/device'
 require 'puppet/provider/force10_interface/dell_ftos'
 require 'pp'
-#require 'spec_lib/puppet_spec/deviceconf'
-#include PuppetSpec::Deviceconf
 
+describe "Integration test for force 10 interface" do
 
-describe "Integration test for force 10 alias" do
-
-  #device_conf =  YAML.load_file(my_deviceurl('force10','device_conf.yml'))    
   provider_class = Puppet::Type.type(:force10_interface).provider(:dell_ftos)
 
   before do
-    #Facter.stub(:value).with(:url).and_return(device_conf['url'])
-    @device = provider_class.device("telnet://admin:Dell_123$@172.152.0.24/?enable=Dell_123$")   
-  end  
-  
-  
+
+    @device = provider_class.device("telnet://admin:Dell_123$@172.152.0.24/?enable=Dell_123$")
+  end
 
   let :force10_interface do
     Puppet::Type.type(:force10_interface).new(
@@ -29,12 +23,11 @@ describe "Integration test for force 10 alias" do
     )
   end
 
-  context 'when configuring interface' do 
-    it "should configure interface" do 
+  context 'when configuring interface' do
+    it "should configure interface" do
       preresult = provider_class.lookup(@device, force10_interface[:name])
       @device.switch.interface(force10_interface[:name]).update(preresult,{:mtu => force10_interface[:mtu], :shutdown => force10_interface[:shutdown]})
       postresult = provider_class.lookup(@device, force10_interface[:name])
-	pp "postresult = #{postresult}"
       postresult.should include({:mtu => force10_interface[:mtu], :shutdown => force10_interface[:shutdown]})
     end
   end
--- spec/integration/puppet/provider/force10_portchannel/dell_ftos_spec.rb
+++ spec/integration/puppet/provider/force10_portchannel/dell_ftos_spec.rb
@@ -4,15 +4,14 @@ require 'spec_provider_helper'
 require 'puppet/util/network_device/dell_ftos/device'
 require 'puppet/provider/force10_portchannel/dell_ftos'
 
-
 describe "Integration test for IOA Interface" do
 
   provider_class = Puppet::Type.type(:force10_portchannel).provider(:dell_ftos)
 
   before do
     #Facter.stub(:value).with(:url).and_return(device_conf['url'])
-    @device = provider_class.device("ssh://admin:Dell_123$@172.152.0.23")   
-  end  
+    @device = provider_class.device("ssh://admin:Dell_123$@172.152.0.23")
+  end
 
   let :config_force10_portchannel do
     Puppet::Type.type(:force10_portchannel).new(
@@ -24,14 +23,11 @@ describe "Integration test for IOA Interface" do
     )
   end
 
-
-
-  context 'when configuring portchannel' do 
-    it "should configure force10 portchannel" do 
+  context 'when configuring portchannel' do
+    it "should configure force10 portchannel" do
       preresult = provider_class.lookup(@device, config_force10_portchannel[:name])
 
       @device.switch.portchannel(config_force10_portchannel[:name]).update(preresult,{:ensure => :present, :desc => config_force10_portchannel[:desc], :mtu => config_force10_portchannel[:mtu], :shutdown =>config_force10_portchannel[:shutdown]})
-
 
       postresult = provider_class.lookup(@device, config_force10_portchannel[:name])
       postresult.should eq({:ensure => :present, :desc => config_force10_portchannel[:desc], :mtu => config_force10_portchannel[:mtu], :shutdown => config_force10_portchannel[:shutdown]})
@@ -39,5 +35,4 @@ describe "Integration test for IOA Interface" do
   end
 
 end
-
 
