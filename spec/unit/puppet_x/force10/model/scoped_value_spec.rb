ffeb05a6ec8fa38571956b4e339d08ef5d31d1f4
R.I.Pienaar <rip@devco.net>
2016-03-17 10:12:16 +0000

    ASM-6191: Do not fail on name errors
    This happens when a VLAN called something already exist, skip these
    errors in all cases

--- spec/unit/puppet_x/force10/model/scoped_value_spec.rb
+++ spec/unit/puppet_x/force10/model/scoped_value_spec.rb
@@ -3,6 +3,18 @@ require 'puppet_x/force10/model/scoped_value'
 describe PuppetX::Force10::Model::ScopedValue do
   let(:sv) { PuppetX::Force10::Model::ScopedValue.new("name", double("transport"), double("facts"), nil) }
 
+  describe "#parseforerror" do
+    it "should not raise on duplicate name errors" do
+      sv.parseforerror("Error: Name already exists.", "add the property value for the parameter 'name'")
+    end
+
+    it "should raise for other errors" do
+      expect {
+        sv.parseforerror("Error: rspec error.", "add the property value for the parameter 'name'")
+      }.to raise_error("Unable to add the property value for the parameter 'name'.Reason:rspec error.")
+    end
+  end
+
   describe "#parse_interface_value" do
     it "should return list of 3 interfaces" do
       interface_value = "0/1,3,5"
